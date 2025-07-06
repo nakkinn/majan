68e1d7209f896369eb9b0b79665dd508a9be44dc
nakkinn <81859514+nakkinn@users.noreply.github.com>
2025-07-07 00:50:18 +0900

    Add files via upload

--- script1.js
+++ script1.js
@@ -17,6 +17,7 @@ const btn1 = document.getElementById("btn1");
 
 area1.addEventListener("pointerdown", ()=>{
     mode = 1;
+    score1p.textContent = "999";
 });
 
 area2.addEventListener("pointerdown", ()=>{
@@ -33,6 +34,7 @@ area4.addEventListener("pointerdown", ()=>{
 
 
 area1.addEventListener("pointerup", ()=>{
+    score1p.textContent = "888";
     if(mode!=-1 && mode!=1){
         to = 1;
         input1.focus();
